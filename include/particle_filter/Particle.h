08fc7d948aca6319dd401474633946ccc83ae895
Luca Morreale <luca.morreale93@gmail.com>
2018-02-01 15:53:09 +0100

    Create source file for Particle class

--- include/particle_filter/Particle.h
+++ include/particle_filter/Particle.h
@@ -9,8 +9,10 @@
 
 #include <eigen3/Eigen/Dense>
 
+typedef Eigen::Matrix<float, 2, 1> Vec2f;
 typedef Eigen::Matrix<float, 5, 1> Vec5f;
-
+typedef Eigen::Matrix<float, 5, 5> Mat5f;
+typedef Eigen::Matrix<float, 1, 3> RowVec3f;
 
 class Particle{
 private:
@@ -23,114 +25,40 @@ private:
     Eigen::Matrix<float, 5, 5> P;  // initial uncertainty
     Eigen::Matrix<float, 5, 5> F;  // Transition Matrix
     Eigen::Matrix<float, 5, 5> Q;  // process noise matrix
+
+
 public:
 
-    Particle(const Particle& that) {
-        this->state = that.state;
-        this->u = that.u;
-        this->H = that.H;
-        this->I = that.I;
-        this->R = that.R;
-        this->P = that.P;
-        this->F = that.F;
-        this->Q = that.Q;
-        this->weight = that.weight;
-    }
-
-    Particle(Vec5f state): weight(0), state(state){
-
-        u << 0,0,0,0,0;
-
-        H << 1,0,0,0,0,           // measurement function 5 states - 3 observed (x, y, theta)
-             0,1,0,0,0,
-             0,0,1,0,0;  
-
-        I << 1,0,0,0,0,           // identity matrix
-             0,1,0,0,0,
-             0,0,1,0,0,
-             0,0,0,1,0,
-             0,0,0,0,1;
-    
-
-        R << 0.5,0,0,               // measurement uncertainty (3 uncorrelated measures with uncertainty)
-             0,0.5,0,
-             0,0,0.5;
-
-        P << 1,0,0,0,0,             // initial uncertainty
-             0,1,0,0,0,
-             0,0,1,0,0,
-             0,0,0,1,0,
-             0,0,0,0,1;            
-                                             // Transition Matrix
-        F << 1,   0,   0, 0, 0,             // x
-             0,   1,   0, 0, 0,             // y
-             0,   0,   1, 0, 0,             // theta
-             0.1, 0,   0, 1, 0,             // x_dot
-             0,   0.1, 0, 0, 1;             // y_dot
-
-        Q << 0.1,0,0,0,0,           // process noise matrix
-             0,0.1,0,0,0,
-             0,0,0.01,0,0,
-             0,0,0,0.01,0,
-             0,0,0,0,0.01;
-        }
-
-    ~Particle(){
-    };
-
-    Vec5f getState() {
-        return this->state;
-    }
+    Particle(const Particle& that);
+    Particle(Vec5f state);
+    ~Particle();    
 
     /*
      * Propagate using Kalman Predict equation
      */
-    void propagate(){
-        state = F * state + u;
-        P = F*P*F.transpose() + Q;
-    }
+    void propagate();
 
     // void drift();
 
-    float distance(Eigen::Matrix<float, 1, 3> &measure) {
-        Eigen::Vector2f pose = state.topRows(2);
-        auto m = measure.transpose();
-        Eigen::Vector2f obs = m.topRows(2);
-        return (pose - obs).norm();
-    }
+    float distance(Eigen::Matrix<float, 1, 3> &measure);
 
-    void fillPose(geometry_msgs::Pose &pose) {
-        pose.position.x = state(0);
-        pose.position.y = state(1);
-        pose.orientation.z = state(2);
-    }
+    void fillPose(geometry_msgs::Pose &pose);
 
-    void setWeight(float weight) { this->weight = weight; }
-    float getWeight() { return weight; }
-    void normalizeWeight(float summation) { this->weight /= summation; }
+    void setWeight(float weight);
+    float getWeight();
+    void normalizeWeight(float summation);
+    Vec5f getState();
 
     /*
      * Implements standard Kalman filter update equation
      */
-    void update(Eigen::Matrix<float, 1, 3> &Z){
-        auto Y =  Z.transpose() - (H*state);
-        auto S = ((H*P) * H.transpose()) + R;
-        auto K = (P * H.transpose()) * S.inverse();
-        state = state + (K * Y);
-        P = (I - (K*H)) * P;
-    }
+    void update(Eigen::Matrix<float, 1, 3> &Z);
 
-    Particle* perturbate(float noiseX, float noiseY, float noiseTheta=0.0f) const
-    {
-        Particle* perturbated = new Particle(*this);
-        perturbated->state(0) += noiseX;
-        perturbated->state(1) += noiseY;
-        perturbated->state(2) += noiseTheta;
+    Particle* perturbate(float noiseX, float noiseY, float noiseTheta=0.0f) const;
 
-        return perturbated;
-    }
-
-
+    // new functions
+    bool isInsideConfidenceInterval(Vec2f &person);
+    float confidenceLevel(Vec2f &person);
 };
 
 typedef Particle* ParticlePtr;
