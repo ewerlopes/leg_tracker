85b801103477c403a68099fdc2aa4c48bfd29f4b
ewerlopes <ewerlopes@gmail.com>
2018-02-02 14:45:15 +0100

    adding generative model for leg distances

--- scripts/leg_distance.py
+++ scripts/leg_distance.py
@@ -3,6 +3,7 @@ import rospy
 import tf
 import math
 import copy
+import numpy as np
 
 # Custom messages
 from leg_tracker.msg import Person, PersonArray, Leg, LegArray 
@@ -10,13 +11,265 @@ from visualization_msgs.msg import Marker
 from geometry_msgs.msg import PolygonStamped, Point32
 
 from munkres import Munkres, print_matrix # For the minimum matching assignment problem. To install: https://pypi.python.org/pypi/munkres 
-import numpy as np
+from scipy.stats import beta
+
+from sklearn.externals import joblib
+
+class likelihood:
+    '''Implements a Bernoulli distribution'''
+    
+    def __init__(self, data):
+        """Likelihood for binary data."""
+        self.counts = {s:0 for s in ['0', '1']}
+        self._process_data(data)
+ 
+    def _process_data(self, data):
+        """Process data."""
+        temp = [str(x) for x in data]
+        for s in ['0', '1']:
+            self.counts[s] = temp.count(s)
+
+        if len(temp) != sum(self.counts.values()):
+            raise Exception("Passed data is not all 0`s and 1`s!")
+    
+    def _process_probabilities(self, p0):
+        """Process probabilities."""
+        n0 = self.counts['0']
+        n1 = self.counts['1']
+
+        if p0 != 0 and p0 != 1:
+            # typical case
+            logpr_data = n0*np.log(p0) + n1*np.log(1.-p0)
+            pr_data = np.exp(logpr_data)
+        elif p0 == 0 and n0 != 0:
+            # p0 can't be 0 if n0 is not 0
+            logpr_data = -np.inf
+            pr_data = np.exp(logpr_data)
+        elif p0 == 0 and n0 == 0:
+            # data consistent with p0=0
+            logpr_data = n1*np.log(1.-p0)
+            pr_data = np.exp(logpr_data)            
+        elif p0 == 1 and n1 != 0:
+            # p0 can't be 1 if n1 is not 0
+            logpr_data = -np.inf
+            pr_data = np.exp(logpr_data)
+        elif p0 == 1 and n1 == 0:
+            # data consistent with p0=1
+            logpr_data = n0*np.log(p0)
+            pr_data = np.exp(logpr_data)
+
+        return pr_data, logpr_data
+        
+    def prob(self, p0):
+        """Get probability of data."""
+        pr_data, _ = self._process_probabilities(p0)
+        return pr_data
+    
+    def log_prob(self, p0):
+        """Get log of probability of data."""
+        _, logpr_data = self._process_probabilities(p0)
+
+        return logpr_data
+
+
+class prior:
+    '''Beta prior for binary data.'''
+    
+    def __init__(self, alpha0=1, alpha1=1):
+        self.a0 = alpha0
+        self.a1 = alpha1
+        self.p0rv = beta(self.a0, self.a1)
+
+    def interval(self, prob):
+        """End points for region of pdf containing `prob` of the
+        pdf-- this uses the cdf and inverse.
+        
+        Ex: interval(0.95)
+        """
+        return self.p0rv.interval(prob)
+
+    def mean(self):
+        """Returns prior mean."""
+        return self.p0rv.mean()
+
+    def pdf(self, p0):
+        """Probability density at p0."""
+        return self.p0rv.pdf(p0)
+
+    def plot(self, ax=''):
+        """A plot showing mean and 95% credible interval."""
+        fig = ""
+        if ax == '':
+            fig, ax = plt.subplots(1, 1)
+        else:
+            x = np.arange(0., 1., 0.01)
+
+            # get prior mean p0
+            mean = self.mean()
+
+            # get low/high pts containg 95% probability
+            low_p0, high_p0 = self.interval(0.95)
+            x_prob = np.arange(low_p0, high_p0, 0.01)
+
+            # plot pdf
+            ax.plot(x, self.pdf(x), 'r-')
+
+            # fill 95% region
+            ax.fill_between(x_prob, 0, self.pdf(x_prob),
+                            color='red', alpha='0.2' )
+
+            # mean
+            ax.stem([mean], [self.pdf(mean)], linefmt='r-', 
+                    markerfmt='ro', basefmt='w-')
+
+            ax.set_xlabel('Probability of Zero')
+            ax.set_ylabel('Prior PDF')
+            ax.set_ylim(0., 1.1*np.max(self.pdf(x)))
+
+        if ax == '':
+            plt.show()
+
+
+class posterior:
+    '''Implements a Beta-Bernoulli model'''
+    def __init__(self, data, prior):
+        """The posterior.
+        data: a data sample as list
+        prior: an instance of the beta prior class
+        """
+        self.likelihood = likelihood(data)
+        self.prior = prior
+        self._process_posterior()
+
+    def _process_posterior(self):
+        """Process the posterior using passed data and prior."""
+
+        # extract n0, n1, a0, a1 from likelihood and prior
+        self.n0 = self.likelihood.counts['0']
+        self.n1 = self.likelihood.counts['1']
+        self.a0 = self.prior.a0
+        self.a1 = self.prior.a1
+        self.p0rv = beta(self.a0 + self.n0, self.a1 + self.n1)
+    
+    def interval(self, prob):
+        """End points for region of pdf containing `prob` of the
+        pdf.
+        Ex: interval(0.95)
+        """
+        return self.p0rv.interval(prob)
+
+    def mean(self):
+        """Returns posterior mean."""
+        return self.p0rv.mean()
+
+    def pdf(self, p0):
+        """Probability density at p0."""
+        return self.p0rv.pdf(p0)
+
+    def plot(self, figSize=(8,3), supertitle='', left=0.12, right=0.97,
+                    bottom=0.21, top=.9, wspace=0.5):
+        """A plot showing prior, likelihood and posterior."""
+        
+        fig = plt.figure(figsize=figSize)
+        fig.subplots_adjust(left=left, right=right, bottom=bottom, top=top, wspace=wspace)
+
+        x = np.arange(0., 1., 0.01)
+        
+        ax0= plt.subplot(131)
+
+        ## Prior
+        # get prior mean p0
+        pri_mean = self.prior.mean()
+
+        # get low/high pts containg 95% probability
+        pri_low_p0, pri_high_p0 = self.prior.interval(0.95)
+        pri_x_prob = np.arange(pri_low_p0, pri_high_p0, 0.01)
+
+        # plot pdf
+        ax0.plot(x, self.prior.pdf(x), 'r-')
+
+        # fill 95% region
+        ax0.fill_between(pri_x_prob, 0, self.prior.pdf(pri_x_prob),
+                           color='red', alpha='0.2' )
+
+        # mean
+        ax0.stem([pri_mean], [self.prior.pdf(pri_mean)],
+                   linefmt='r-', markerfmt='ro',
+                   basefmt='w-')
+
+        ax0.set_title('Prior PDF')
+        ax0.set_ylabel('$p(x)$')
+        ax0.set_ylim(0., 1.1*np.max(self.prior.pdf(x)))
+
+        ax1= plt.subplot(132)
+        ## Likelihood
+        # plot likelihood
+        lik = [self.likelihood.prob(xi) for xi in x]
+        ax1.plot(x, lik, 'k-')
+        ax1.set_ylabel('$p(x)$')
+        ax1.set_title('Likelihood')
+
+        ax2= plt.subplot(133)
+        ## Posterior
+        # get posterior mean p0
+        post_mean = self.mean()
+
+        # get low/high pts containg 95% probability
+        post_low_p0, post_high_p0 = self.interval(0.95)
+        post_x_prob = np.arange(post_low_p0, post_high_p0, 0.01)
+
+        # plot pdf
+        ax2.plot(x, self.pdf(x), 'b-')
+
+        # fill 95% region
+        ax2.fill_between(post_x_prob, 0, self.pdf(post_x_prob),
+                           color='blue', alpha='0.2' )
+
+        # mean
+        ax2.stem([post_mean], [self.pdf(post_mean)],
+                   linefmt='b-', markerfmt='bo',
+                   basefmt='w-')
+
+        ax2.set_xlabel('Probability of Zero')
+        ax2.set_ylabel('$p(x)$')
+        ax2.set_title('Posterior PDF')
+        ax2.set_ylim(0., 1.1*np.max(self.pdf(x)))
+
+        plt.suptitle(supertitle)
+        plt.show()
+
+class LegContextProcessor:
+    ''' 
+    Process pairs of leg clusters and weight them by their
+    probability of being a human using a generative model trained
+    by data.
+    
+    Additionally, use a beta-bernoulli model for controlling the
+    combinatorial explosion on the number of leg pair evaluations.
+    
+    '''
+    
+    def __init__(self, model_name):
+        self.model = joblib.load(model_name)
+        self.cutoff = prior(1, 1)                # prior with uniform parameters
+
+    
+    def updateCutoff(self, data):
+        '''Updates the cutoff probability for the getDistance() method'''
+        self.cutoff = posterior(data, self.cutoff)
+    
+    def getProbability(self, distance):
+        '''Evaluate pair distance probability'''
+        logprob = self.model.score_samples(np.array(distance))   # get log-probability
+        return np.exp(logprob)
 
 class LegDistance: 
 
     '''Calculates the distance between LEG clusters and saves to file.''' 
     
-    def __init__(self, x_min, x_max, y_min, y_max, logfile="/home/airlab/Desktop/file.txt"):
+    def __init__(self, x_min, x_max, y_min, y_max, logfile=''):
+
+        self.leg_context = LegContextProcessor('/home/airlab/catkin_ws/src/phd_robogame/perception/player_tracker/model/leg_distance-gmm.pkl')
 
         self.tf_listener = tf.TransformListener()
         self.marker_pub = rospy.Publisher('detected_legs_in_bounding_box', Marker, queue_size=300)
@@ -122,22 +375,48 @@ class LegDistance:
             out = abs(z.T-z)
             out[np.triu_indices(out.shape[0])] = 500     # 500 is a big weight that replaces inf.
             out_backup = copy.deepcopy(out)
-            print np.array_str(out, precision=2, suppress_small=True)
+            #print np.array_str(out, precision=2, suppress_small=True)
 
             # Run munkres on match_dist to get the lowest cost assignment
             munkres = Munkres()
             indexes = munkres.compute(out_backup)
 
-            if self.isSaveToFile:
-                N_people_hat = int(math.floor(len(accepted_clusters)/2))         # estimated number of people from number of legs inside bounding box.   
-                    
+            
+            N_people_hat = int(math.floor(len(accepted_clusters)/2))         # estimated number of people from number of legs inside bounding box.   
+
+            if self.isSaveToFile:   
                 for row, column in indexes[1:N_people_hat+1]:
-                    rospy.logerr('{}-{}'.format(row, column))
                     self.f.write(str(out[row][column])+'\n')
                     self.f.flush()
+            else:
+                for row, column in indexes[1:N_people_hat+1]:
 
-        except Exception as e:
-            rospy.logwarn('Distance matrix shape: {}'.format(out.shape))
+                    ### PUBLISHES A RED SPHERE MARKER WITH VARUABLE COLOR (BASED ON DISTANCE PROBABILITY) for each per of leg found ####
+                    for j, m in enumerate([row, column]):
+                        # publish rviz markers       
+                        marker = Marker()
+                        marker.header.frame_id = self.fixed_frame
+                        marker.header.stamp = now
+                        marker.id = j
+                        marker.ns = "detected_legs"                       
+                        marker.type = Marker.SPHERE
+                        marker.scale.x = 0.2
+                        marker.scale.y = 0.2
+                        marker.scale.z = 0.2
+                        marker.color.r = self.leg_context.getProbability(out[row][column])
+                        marker.color.g = 0
+                        marker.color.b = 0
+                        marker.color.a = 1
+                        marker.pose.position.x = accepted_clusters[m].position.x
+                        marker.pose.position.y = accepted_clusters[m].position.y        
+                        marker.pose.position.z = 0.1                        
+                        marker.lifetime = rospy.Duration(0.1)
+
+                        # Publish to rviz and /people_tracked topic.
+                        self.marker_pub.publish(marker)
+                    rospy.logwarn(self.leg_context.getProbability(out[row][column]))
+        except IndexError as ex:
+            pass
         
 
 
