54f4412044322b03e48f31a22960c025bb34ade4
Luca Morreale <luca.morreale93@gmail.com>
2018-02-20 16:28:03 +0100

    Fix perception grid matching algorithm

--- scripts/perception_grid_match.py
+++ scripts/perception_grid_match.py
@@ -10,11 +10,20 @@ class BRIEFWrapper(object):
         self.star = cv2.xfeatures2d.StarDetector_create() # Initiate FAST detector
         self.brief = cv2.xfeatures2d.BriefDescriptorExtractor_create() # Initiate BRIEF extractor
 
-    def detectAndCompute(self, image):
+    def detectAndCompute(self, image, void):
         keypoints = self.star.detect(image, None)  # find the keypoints with STAR
         keypoints, descriptors = self.brief.compute(image, keypoints) # compute the descriptors with BRIEF
         return keypoints, descriptors
 
+class FREAKWrapper(object):
+    def __init__(self):
+        self.star = cv2.xfeatures2d.StarDetector_create()
+        self.freak = cv2.xfeatures2d.FREAK_create()
+
+    def detectAndCompute(self, image, void):
+        keypoints = self.star.detect(image, None)
+        keypoints, descriptors = self.freak.compute(image, keypoints)
+        return keypoints, descriptors
 
 class PerceptionGridProjector(object):
     keypoints = {}
@@ -33,10 +42,11 @@ class PerceptionGridProjector(object):
         elif detector_name == 'SURF':
             self.detector = cv2.xfeatures2d.SURF_create()
             match_distance = cv2.NORM_L2
-        elif detector_name == 'FAST':
-            self.detector = cv2.FastFeatureDetector()
-            match_distance = cv2.NORM_L2
-
+        elif detector_name == 'FREAK':
+            self.detector = FREAKWrapper()
+            match_distance = cv2.NORM_HAMMING
+        else:
+            raise Exception('The detector selected does not exist')
         # create BFMatcher object
         self.matcher = cv2.BFMatcher(match_distance, crossCheck=True)
 
@@ -51,7 +61,23 @@ class PerceptionGridProjector(object):
 
     def compute_transformation(self, matches, keypoints1, keypoints2):
         pts1, pts2 = self.extract_points(matches, keypoints1, keypoints2)
-        return cv2.getAffineTransform(pts1, pts2)
+        M = cv2.getAffineTransform(pts1, pts2)
+        M[:, :-1] = self.gs(M[:, :-1]) # normalization of rotation matrix
+        return M
+
+    def gs(self, X, row_vecs=True, norm = True):
+        if not row_vecs:
+            X = X.T
+        Y = X[0:1,:].copy()
+        for i in range(1, X.shape[0]):
+            proj = np.diag((X[i,:].dot(Y.T)/np.linalg.norm(Y,axis=1)**2).flat).dot(Y)
+            Y = np.vstack((Y, X[i,:] - proj.sum(0)))
+        if norm:
+            Y = np.diag(1/np.linalg.norm(Y,axis=1)).dot(Y)
+        if row_vecs:
+            return Y
+        else:
+            return Y.T
 
     def project_points(self, M, points):
         rows = len(points)
@@ -72,7 +98,7 @@ class PerceptionGridProjector(object):
         for match in matches:
             pts1 = self.append_point_to(kp1[match.trainIdx], pts1)
             pts2 = self.append_point_to(kp2[match.queryIdx], pts2)
-        return pts1[3:6, :], pts2[3:6, :]
+        return pts1[:3, :], pts2[:3, :]
 
     def append_point_to(self, keypoint, matrix):
         pt = list(keypoint.pt)
@@ -81,73 +107,55 @@ class PerceptionGridProjector(object):
         else:
             return np.append(matrix, np.float32([pt]), axis=0)
 
-    def store(self, image, kp, des):
-        self.keypoints[image] = kp
-        self.descriptors[image] = des
-
-    def clear(self):
-        self.keypoints = {}
-        self.descriptors = {}
-
-    def delete(self, image):
-        del self.keypoints[image]
-        del self.descriptors[image]
-
     def get_rotation_matrix(self, map_kp, map_desc, perc_kp, perc_desc):
         matches = self.compute_matches(map_desc, perc_desc)
         rot_matrix = self.compute_transformation(matches, map_kp, perc_kp)
         return rot_matrix
 
+def show_image(window_name, img, click=False):
+    cv2.namedWindow(window_name,cv2.WINDOW_NORMAL)
+    if click:
+        cv2.setMouseCallback(window_name, mouse_callback) #Mouse callback
+    cv2.imshow(window_name, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+   # mouse callback function
+def mouse_callback(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print x, y
+
+
 if __name__ == "__main__":
 
-    perception_grid_projector = PerceptionGridProjector()
+    perception_grid_projector = PerceptionGridProjector('ORB')
 
-    # Load an color image in grayscale 
-    ### CHANGE PATH FOR FILES ### 
-    map_img = cv2.imread('/home/airlab/catkin_ws/src/player_tracker/imagesimage.png',0)
+    # Load an color image in grayscale
+    ### CHANGE PATH FOR FILES ###
+    # map_img = cv2.imread('/home/luca/projects/people_tracking/src/leg_tracker/images/image.png',0)
+    map_img = cv2.imread('/home/luca/projects/people_tracking/src/leg_tracker/images/image.png', 0)
 
-    cv2.namedWindow('mapimage', cv2.WINDOW_NORMAL)
-    cv2.imshow('mapimage',map_img)
-    cv2.waitKey(0)
+    show_image('mapimage', map_img)
 
     # map descriptor
     map_kp, map_desc = perception_grid_projector.compute_descriptors(map_img)
 
     # image descriptor
-    img = cv2.imread('/home/airlab/catkin_ws/src/player_tracker/images/image_grid.png',0)
+    grid_img = cv2.imread('/home/luca/projects/people_tracking/src/leg_tracker/images/image_grid.png', 0)
 
-    cv2.namedWindow('image_grid', cv2.WINDOW_NORMAL)
-    cv2.imshow('image_grid', img)
-    cv2.waitKey(0)
+    show_image('image_grid', grid_img)
 
     # map descriptor
-    perc_kp, perc_desc = perception_grid_projector.compute_descriptors(img)
+    perc_kp, perc_desc = perception_grid_projector.compute_descriptors(grid_img)
 
-    rotation_mtx = perception_grid_projector.get_rotation_matrix(map_kp, map_desc, perc_kp, perc_desc)
-    trans_ = perception_grid_projector.project_image(rotation_mtx, img)
+    rotation_mtx = perception_grid_projector.get_rotation_matrix(perc_kp, perc_desc, map_kp, map_desc)
 
-    cv2.namedWindow('trans_', cv2.WINDOW_NORMAL)
-    cv2.imshow('trans_', trans_)
-    cv2.waitKey(0)
+    trans_ = perception_grid_projector.project_image(rotation_mtx, grid_img)
+    show_image('trans_', trans_)
+    print trans_.shape
 
-    matches = perception_grid_projector.compute_matches(map_desc, perc_desc)
-
-    #draw_params = dict(matchColor=(0,255,0), singlePointColor=(255,0,0), flags=0)
-    #img3 = cv2.drawMatchesKnn(map_img, map_kp, img, perc_kp, matches, None, flags=2)
-    print len(matches)
-    print len(map_kp)
-    print len(perc_kp)
-    img3 = cv2.drawMatches(img, perc_kp, map_img, map_kp, matches[:10], None, flags=2)
-
-    cv2.namedWindow('img3', cv2.WINDOW_NORMAL)
-    cv2.imshow('img3',img3)
-    cv2.waitKey(0)
-
-    print np.array_str(trans_, precision=2, suppress_small=True)
-    print np.where(trans_ > 0)
-
-    print "image: {}".format(np.where(img > 0))
-    print "map: {}".format(np.where(map_img > 0))
+    print "image:  {}".format(np.where(grid_img > 0)[0].shape)
+    print "map:    {}".format(np.where(map_img > 0)[0].shape)
+    print "trans_: {}".format(np.where(trans_ > 0)[0].shape)
     print "rotation: \n{}".format(np.array_str(rotation_mtx, precision=2, suppress_small=True))
 
-    #cv2.imwrite('/home/airlab/Scrivania/trans_.png',trans_)
