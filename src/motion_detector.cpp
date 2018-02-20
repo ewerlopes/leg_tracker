2f3128d1835cc0a7189828787f69fc747aa02973
ewerlopes <ewerlopes@gmail.com>
2018-02-20 18:59:43 +0100

    backup

--- src/motion_detector.cpp
+++ src/motion_detector.cpp
@@ -13,6 +13,10 @@
 #include <sensor_msgs/LaserScan.h>
 #include <sensor_msgs/PointCloud.h>
 
+// basic file operations
+#include <iostream>
+#include <fstream>
+
 /**
 * @basic The motion detector algorithm in Shen, Xiaotong, Seong-Woo Kim, and
 * Marcelo H. Ang.
@@ -29,7 +33,7 @@ public:
     * @param nh A nodehandle
     * @param scan_topic The topic for the scan we would like to map
     */
-    MotionDetector(ros::NodeHandle nh, std::string scan_topic, float window_duration)
+    MotionDetector(ros::NodeHandle nh, std::string scan_topic, float window_duration, std::string log_filename)
         : nh_(nh), scan_topic_(scan_topic), window_duration_(window_duration), on_window(false) {
         ros::NodeHandle nh_private("~");
         base_frame_ = "odom";
@@ -39,6 +43,12 @@ public:
 
         ros::service::waitForService("assemble_scans");
         client = nh_.serviceClient<laser_assembler::AssembleScans>("assemble_scans");
+
+        log_file.open (log_filename);
+    }
+
+    ~MotionDetector(){
+        log_file.close();
     }
 
 private:
@@ -47,6 +57,8 @@ private:
     std::string base_frame_;
 
     bool on_window;
+
+    std::ofstream log_file;
 
     ros::Duration window_duration_;// delta_t
     ros::Time start_time;          // t_s
@@ -76,6 +88,16 @@ private:
     }
 
     void publishMergedCloud(ros::Time from, ros::Time to){
+        sensor_msgs::PointCloud cloud;
+
+        if (mergeCloud(from, to, cloud)){
+            merged_cloud_pub.publish(cloud);
+        }else{
+            ROS_ERROR("Failed to publish merged cloud!");
+        }
+    }
+
+    int mergeCloud(ros::Time from, ros::Time to, sensor_msgs::PointCloud &cloud){
         srv.request.begin = from;
         srv.request.end   = to;
         
@@ -83,9 +105,11 @@ private:
 
         if (client.call(srv)){
             ROS_INFO("Got cloud with %lu points\n", srv.response.cloud.points.size());
-                merged_cloud_pub.publish(srv.response.cloud);
+            cloud = srv.response.cloud;
+            return 1;
         }else{
             ROS_ERROR("Service call to assemble_scans failed\n");
+            return 0;
         }
     }
 
@@ -93,6 +117,13 @@ private:
         #pragma omp parallel for    
         for (int i=0; i < cloud.points.size(); i++){
             cloud.points[i].z = time;
+        }
+    }
+
+    void saveToLog(sensor_msgs::PointCloud &cloud, double time){
+        #pragma omp parallel for    
+        for (int i=0; i < cloud.points.size(); i++){
+            log_file << cloud.points[i].x << "," << cloud.points[i].y << "," << cloud.points[i].z << std::endl;
         }
     }
 
@@ -119,17 +150,22 @@ private:
         }else if ((scan_msg->header.stamp.toSec() - start_time.toSec()) < window_duration_.toSec()){
             ROS_INFO("Inside windows...");
             ROS_INFO("Time: %f", (scan_msg->header.stamp.toSec() - start_time.toSec()));
-            getPointCloud(scan_msg,cloud);  
+            getPointCloud(scan_msg,cloud);
         }else{
             ROS_WARN("Resetting...");
+            log_file << "--" << std::endl;
             on_window = false;
-
             return;
         }
 
+        // set time dimension
         setTemporal(cloud, (scan_msg->header.stamp.toSec() - start_time.toSec()));
+        
         //publishMergedCloud(start_time, ros::Time::now());
         publishMergedCloud(ros::Time(0.0), ros::Time::now());
+        
+        // save to file
+        saveToLog(cloud, (scan_msg->header.stamp.toSec() - start_time.toSec()));
 
         // publich point cloud
         cloud_pub.publish(cloud);
@@ -145,7 +181,7 @@ int main(int argc, char **argv) {
     ros::NodeHandle nh;
     std::string scan_topic;
     nh.param("scan_topic", scan_topic, std::string("scan"));
-    MotionDetector md(nh, scan_topic, 3.0);
+    MotionDetector md(nh, scan_topic, 1, "/home/airlab/Scrivania/log_file.txt");
 
     ros::spin();
     return 0;
