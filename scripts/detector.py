311063eb077d00b0d17c9c4db6006f0722778329
ewerlopes <ewerlopes@gmail.com>
2018-02-24 19:27:10 +0100

    Fixing speed field type in Leg message

--- scripts/detector.py
+++ scripts/detector.py
@@ -761,7 +761,7 @@ class Tracker:
 
         # Publish people tracked message
         self.people_tracked_pub.publish(people_tracked_msg)  
-        rospy.logwarn(people_tracked_msg.people)
+        rospy.logwarn(people_tracked_msg)
         self.polar_grid.publish_fov(now)         
 
 if __name__ == '__main__':
